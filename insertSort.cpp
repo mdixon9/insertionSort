2209be006e3c16e3e70d7e6834fa36d985fc3152
Maliek Dixon <maliek.dixon@gmail.com>
2023-07-20 18:18:13 -0400

    Added comments to the program

--- insertSort.cpp
+++ insertSort.cpp
@@ -3,14 +3,17 @@
 /**
  * @brief Implements an insertion sort for an array of integers
  * 
- * @param array
- * @param numItems 
+ * @param array - points to address of first elem in array
+ * @param numItems - number of elements in array
  */
 void insertionSort(int *array, int numItems)
 {
 
-    int innerLoopItems = numItems + 1;     // tracks the inner loop items
-    bool IterationHadASwitch = false;      // checks if there was a switch
+    // tracks the number of inner loop items
+    int innerLoopItems = numItems + 1;    
+
+    // checks if there was a switch
+    bool IterationHadASwitch = false;     
 
     // -----------------------------------------------------------------
     // outer loop: controls each iteration to check if the first iteration 
@@ -18,7 +21,7 @@ void insertionSort(int *array, int numItems)
     // -----------------------------------------------------------------
     for (int i = 0; i < numItems; i++)
     {
-
+        // decrements the # of iterations because greatest # is sorted
         innerLoopItems--;
 
         // -----------------------------------------------------------------
@@ -38,8 +41,9 @@ void insertionSort(int *array, int numItems)
             {
                 continue;
             }
-        }
+        } // END of inner loop
 
+        // 47 - 55 For debugging and stepping through each iteration
         printf("Iteration %d ::: ", i + 1);
 
         for(int m = 0; m < numItems; m++)
@@ -48,7 +52,7 @@ void insertionSort(int *array, int numItems)
         }
 
         printf("\n");
-    }
+    } // END of outer loop
 
     // exits early if array is already sorted
     if (IterationHadASwitch == false)
@@ -56,34 +60,33 @@ void insertionSort(int *array, int numItems)
         printf("Early exit as array is sorted\n");
         return;
     }
-}
+} // END of insertionSort
 
 int main()
 {
-
     // 72, 35, 38, 73, 35, 83, 27, 16, 85
     int arr1[] = {72, 35, 38, 73, 35, 83, 27, 16, 85};
+
+    // gets the number of elems in array
     int arr1Items = sizeof(arr1) / sizeof(arr1[0]);
 
+    // print unsorted array
     printf("The array is: ");
-
     for (int i = 0; i < arr1Items; i++)
     {
         printf("%d ", arr1[i]);
     }
-
     printf("\n\n");
 
-
+    // sorts the array
     insertionSort(arr1, arr1Items);
     
+    // prints the sorted array
     printf("\nThe sorted array is: ");
-
     for (int i = 0; i < arr1Items; i++)
     {
         printf("%d ", arr1[i]);
     }
-
     printf("\n");
     return 0;
 }
